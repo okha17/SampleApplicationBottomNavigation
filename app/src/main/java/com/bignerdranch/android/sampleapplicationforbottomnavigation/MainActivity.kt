71ee6a9c69575bcb62de9160d62780f1eca58569
Osman Khan <osmankhan902@gmail.com>
2021-11-05 12:55:24 -0700

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/sampleapplicationforbottomnavigation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.sampleapplicationforbottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.itemIconTintList = null;
+    }
+}
