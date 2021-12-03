ae9736ff77f34412a5f2208966fd47e8db2b78aa
Osman Khan <osmankhan902@gmail.com>
2021-12-02 21:14:02 -0800

    Test

--- app/src/main/java/com/bignerdranch/android/sampleapplicationforbottomnavigation/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/sampleapplicationforbottomnavigation/MainActivity.kt
@@ -2,16 +2,41 @@ package com.bignerdranch.android.sampleapplicationforbottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.bignerdranch.android.sampleapplicationforbottomnavigation.fragments.DashFragment
+import com.bignerdranch.android.sampleapplicationforbottomnavigation.fragments.SettingsFragment
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_dash.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
+    private val dahsboardFragment = DashFragment()
+    private val settingsFragment = SettingsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottomNavigationView = findViewById(R.id.bottom_navigation)
-
         bottomNavigationView.itemIconTintList = null;
+
+        replaceFragment(dahsboardFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_logo -> replaceFragment(dahsboardFragment)
+                R.id.ic_quick_pc -> replaceFragment(settingsFragment)
+            }
+            true
+        }
     }
+
+    private fun replaceFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+
+
 }
